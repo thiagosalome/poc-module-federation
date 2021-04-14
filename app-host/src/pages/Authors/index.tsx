ad925729792bf5817efec15b19ca669f0c60aee7
Thiago Salome <thiagosalome@gmail.com>
2021-04-14 07:57:14 -0300

    Adicionando app host

---
+++ app-host/src/pages/Authors/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Authors = () => {
+  return (
+    <p>Autores</p>
+  )
+}
+
+export default Authors;
---
+++ app-host/src/routes.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+// Pages
+import Home from 'src/pages/Home'
+import Authors from 'src/pages/Authors'
+import Books from 'src/pages/Books'
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/authors' component={Authors} />
+        <Route path='/books' component={Books} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default Routes;
