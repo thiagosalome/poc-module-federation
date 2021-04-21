116461ed5dc596e18fd2fa37a8e3688689188c80
Thiago Salome <thiagosalome@gmail.com>
2021-04-21 12:42:03 -0300

    Subindo pasta dist

--- host-application/config/webpack.common.js
+++ host-application/config/webpack.common.js
@@ -2,6 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ModuleFederation = require('webpack').container.ModuleFederationPlugin
 const dependencies = require('../package.json').dependencies
+require('dotenv-flow').config()
 
 module.exports = {
   entry: path.resolve('./src/index.tsx'),
@@ -26,8 +27,8 @@ module.exports = {
       name: 'application_host',
       filename: 'remoteEntry.js',
       remotes: {
-        authors_application: 'authors_application@http://localhost:3001/remoteEntry.js',
-        books_application: 'books_application@http://localhost:3002/remoteEntry.js'
+        authors_application: `authors_application@${process.env.AUTHORS_APPLICATION}/remoteEntry.js`,
+        books_application: `books_application@${process.env.BOOKS_APPLICATION}/remoteEntry.js`
       },
       shared: {
         ...dependencies,
