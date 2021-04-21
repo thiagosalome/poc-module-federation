189b801dc677fe17671cae57881ce6770c67fc65
Thiago Salome <thiagosalome@gmail.com>
2021-04-21 08:31:53 -0300

    Substituindo app-host por host-application

--- app-host/src/pages/Authors/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Authors = () => {
-  return (
-    <p>Autores</p>
-  )
-}
-
-export default Authors;
--- app-host/src/pages/Books/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Books = () => {
-  return (
-    <p>Livros</p>
-  )
-}
-
-export default Books;
